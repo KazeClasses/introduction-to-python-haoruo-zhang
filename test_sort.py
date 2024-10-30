7e0ad1b0ee3d06b9a8ec8f934d39698830c92a1a
Haoruo <hzhan237@jh.edu>
2024-10-29 23:45:30 -0400

    upgrade

--- test_sort.py
+++ test_sort.py
@@ -5,8 +5,16 @@ N = 100
 random.seed(int(time.time()))
 
 def insertion_sort(unsorted_list: list[int]) -> list[int]:
-    # Implement your function here ...
-    raise NotImplementedError
+    for i in range(1, len(unsorted_list)):
+        key = unsorted_list[i]
+        j = i - 1
+        # Move elements of unsorted_list[0...i-1], that are greater than key,
+        # to one position ahead of their current position
+        while j >= 0 and key < unsorted_list[j]:
+            unsorted_list[j + 1] = unsorted_list[j]
+            j -= 1
+        unsorted_list[j + 1] = key
+    return unsorted_list
 
 # Do not change the following lines
 def test_insertion_sort():
